3ab7f14fef4f27a4b9a20f224cd91b77339e8f20
okutani-t <okutani.nt@gmail.com>
2022-06-28 18:29:47 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import img from './images/img.png'
+
+const App = () => {
+  return (
+    <>
+      <img src={img} alt="羊の画像です。" />
+      <h1>Hello Vite + React!</h1>
+    </>
+  )
+}
+
+export default App
